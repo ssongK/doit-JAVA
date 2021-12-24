ba0281ee2078230b816b83735f242f6bed540cf5
ssongK <chansong9840@naver.com>
2021-12-24 15:33:32 +0900

    first commit

--- classCreate.java
+++ classCreate.java
@@ -1,21 +1,21 @@
-class A {
-    int m = 3;
-    int n = 4;
-    void work1(){
-        int k = 5;
-        System.out.println(k);
+class A {   // 클래스 생성
+    int m = 3;   // 필드
+    int n = 4;   // 필드
+    void work1(){  
+        int k = 5;   // 지역 변수
+        System.out.println(k);   // 5
         work2(3);
     }
     void work2(int i){
         int j = 4;
-        System.out.println(i + j);
+        System.out.println(i + j); // 7
     }
 }
-public class test{
+public class classCreate{
     public static void main(String[] args){
         A a = new A();
-        System.out.println(a.m);
-        System.out.println(a.n);
+        System.out.println(a.m);  // 3
+        System.out.println(a.n);  // 4
         a.work1();
     }
 }
---
+++ exturnalMethod.java
@@ -0,0 +1,39 @@
+
+class B{
+    // 리턴 타입 void, 입력매개변수 없음
+    void print(){
+        System.out.println("안녕");
+    }
+    // 리턴 타입 int, 입력매개변수 없음
+    int data(){
+        return 3;
+    }
+    // 리턴 타입 double, 입력매개변수 2개
+    double sum(int a, double b){
+        return a+b;
+    }
+    // 리턴 타입 void, 입력매개변수 1개, 내부에 리턴 포함(함수종료)
+    void printMonth(int m){
+        if(m<0||m>12){
+            System.out.println("잘못된 입력");
+            return;
+        }
+        System.out.println(m+"월 입니다.");
+    }
+}
+
+public class exturnalMethod{
+    public static void main(String[] arg){
+        // 객체 생성
+        B a = new B();
+        // 메서드 호출(멤버 활용)
+        a.print();
+        int k = a.data();
+        a.data();
+        System.out.println(k);
+        double result = a.sum(3,5.2);
+        System.out.println(result);
+        a.printMonth(5);
+        a.printMonth(15);
+    }
+}
